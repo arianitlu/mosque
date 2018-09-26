44e78ca47567bdb22906d11972cc73fa711bb79f
Arianit Lubishtani <arianit.lu@hotmail.com>
2018-09-26 16:10:00 +0200

    Donation Activity added. Also added special layout for the view and fix proper chains between elements.

---
+++ app/src/main/java/com/example/pluscomputers/mosque/DonacionetActivity.java
@@ -0,0 +1,13 @@
+package com.example.pluscomputers.mosque;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonacionetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donacionet);
+    }
+}
