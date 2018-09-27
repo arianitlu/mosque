237477c317d6e59dbb83d3db3adff6bfe718e4b2
Arianit Lubishtani <arianit.lu@hotmail.com>
2018-09-27 19:19:42 +0200

    Added AnetaresiaActivity. Added model (anetaresia class) and adapter (anetaresiaAdapter).

---
+++ app/src/main/java/com/example/pluscomputers/mosque/AnetaresiaActivity.java
@@ -0,0 +1,47 @@
+package com.example.pluscomputers.mosque;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.pluscomputers.mosque.adapters.AnetaresiaAdapter;
+import com.example.pluscomputers.mosque.model.Anetaresia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnetaresiaActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<Anetaresia> anetaresiaList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anetaresia);
+
+        recyclerView = findViewById(R.id.ant_recyclerview);
+
+        AnetaresiaAdapter adapter = new AnetaresiaAdapter(this);
+
+        adapter.setAnetaresia(listAnetaresia());
+
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    public List<Anetaresia> listAnetaresia() {
+
+        Anetaresia obj1 = new Anetaresia("Arianit","Lubishtani",R.mipmap.mosque);
+        anetaresiaList.add(obj1);
+        Anetaresia obj2 = new Anetaresia("Xhemajl","Prekorogja",R.mipmap.ic_launcher);
+        anetaresiaList.add(obj2);
+
+        return anetaresiaList;
+    }
+}
---
+++ app/src/main/java/com/example/pluscomputers/mosque/adapters/AnetaresiaAdapter.java
@@ -0,0 +1,70 @@
+package com.example.pluscomputers.mosque.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.pluscomputers.mosque.R;
+import com.example.pluscomputers.mosque.model.Anetaresia;
+
+import java.util.List;
+
+public class AnetaresiaAdapter extends RecyclerView.Adapter<AnetaresiaAdapter.MyViewHolder> {
+
+    private List<Anetaresia> mAnetaresiaList;
+    private Context ctx;
+
+    public AnetaresiaAdapter(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mEmri, mPozita;
+        ImageView mImage;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            mEmri = itemView.findViewById(R.id.ant_text_view_emri);
+            mPozita = itemView.findViewById(R.id.ant_text_view_pozita);
+
+            mImage = itemView.findViewById(R.id.ant_image_view);
+        }
+    }
+
+    @Override
+    public AnetaresiaAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.ant_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(AnetaresiaAdapter.MyViewHolder holder, int position) {
+        Anetaresia anetaresia = mAnetaresiaList.get(position);
+
+        holder.mEmri.setText(anetaresia.getEmri());
+        holder.mPozita.setText(anetaresia.getPozita());
+        holder.mImage.setImageResource(anetaresia.getImage());
+//        Picasso.with(ctx)
+//                .load(list.getImg())
+//                .into(holder.mImageViewMain);
+    }
+
+    public void setAnetaresia(List<Anetaresia> anetaresiaList){
+        this.mAnetaresiaList = anetaresiaList;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
