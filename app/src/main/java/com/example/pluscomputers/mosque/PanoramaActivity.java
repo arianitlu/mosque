e61697cfae51f4b671da1ed5ad12c626b18edc88
Arianit Lubishtani <arianit.lu@hotmail.com>
2018-09-28 17:24:29 +0200

    Added PanoramaActivity where you can view a panorama photo

--- app/src/main/java/com/example/pluscomputers/mosque/PanoramaActivity.java
+++ app/src/main/java/com/example/pluscomputers/mosque/PanoramaActivity.java
@@ -3,11 +3,35 @@ package com.example.pluscomputers.mosque;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.gjiazhe.panoramaimageview.GyroscopeObserver;
+import com.gjiazhe.panoramaimageview.PanoramaImageView;
+
 public class PanoramaActivity extends AppCompatActivity {
+
+    private GyroscopeObserver gyroscopeObserver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panorama);
+
+        gyroscopeObserver = new GyroscopeObserver();
+        //gyroscopeObserver.setMaxRotateRadian(Math.PI/9);
+        gyroscopeObserver.setMaxRotateRadian(1);
+
+        PanoramaImageView panoramaImageView = findViewById(R.id.panorama_image_view);
+        panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gyroscopeObserver.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gyroscopeObserver.unregister();
     }
 }
